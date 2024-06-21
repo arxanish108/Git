45f43044ee9cb81eb2baa6422846099070048d8f
Anish Roshan <109199934+arxanish108@users.noreply.github.com>
2024-06-22 00:39:01 +0530

    third with f1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Dunia");
+        System.out.println("Hello We are in f1");
 
     }
 }
